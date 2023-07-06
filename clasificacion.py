7acc63bc930f38737e35f360f371245b61891fc3
TenshiZ <angelant@ucm.es>
2023-07-06 08:43:57 +0000

    clasificacion hecho

--- clasificacion.py
+++ clasificacion.py
@@ -1,42 +1,47 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from xgboost import XGBRegressor
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
+from sklearn.preprocessing import LabelEncoder
 
-
+# Read the dataset from the CSV file
 model = pd.read_csv('Breast_Cancer.csv')
 
+# Split the dataset into features (X) and target variable (y)
+target = 'Status'
+y = model[target]
+X = model.drop([target], axis=1)
 
-target = 'Tumor Size' #regresion ya que no buscamos clasificarlos solo su numero aprox
-y = model[target]            
-X =model.drop([target], axis=1)
+X = pd.get_dummies(X)
 
+# Encode the target variable with numeric values
+le = LabelEncoder()
+y = le.fit_transform(y)
 
-X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, test_size=0.2,random_state=0)
+# Split the data into training and validation sets
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=0)
 
-X_train = pd.get_dummies(X_train_full)
-X_valid = pd.get_dummies(X_valid_full)
+# Train the XGBoost classifier
+my_model = XGBClassifier(n_estimators=1000, learning_rate=0.05, n_jobs=4, early_stopping_rounds=5)
+my_model.fit(X_train, y_train, eval_set=[(X_valid, y_valid)], verbose=False)
 
-X_train, X_valid = X_train.align(X_valid, join='left', axis=1)
-
-my_model = XGBRegressor(n_estimators = 1000, learning_rate = 0.05 , n_jobs = 4, early_stopping_rounds = 5)
-
-my_model.fit(X_train, y_train, eval_set=[(X_valid, y_valid)], verbose = False) 
-
+# Predict the target variable values for the validation set
 predict_model = my_model.predict(X_valid)
 
 
+# Calculate the accuracy
+accuracy = accuracy_score(y_valid, predict_model)
+precision = precision_score(y_valid, predict_model)
+f1 = f1_score(y_valid, predict_model)
 
+predict_model = np.where(predict_model == 0, "Dead", "Alive")
+y_valid = np.where(y_valid == 0, "Dead", "Alive")
 print(predict_model)
 print(y_valid)
 
-mae = mean_absolute_error(y_valid, predict_model)
-mse = mean_squared_error(y_valid, predict_model)
-r2 = r2_score(y_valid, predict_model)
-
-print("Mean Absolute Error (MAE):", mae)
-print("Mean Squared Error (MSE):", mse)
-print("R2 Score:", r2)
+print("Accuracy:", accuracy)
+print("Precision:", precision)
+print("F1_score:", f1)
