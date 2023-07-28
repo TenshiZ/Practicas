b8847f4ac92da4c73478c9de62fada304c37bfd2
TenshiZ <angelant@ucm.es>
2023-07-28 09:05:53 +0000

    new

--- insectos/yolo.py
+++ insectos/yolo.py
@@ -36,7 +36,10 @@ def detect_objects(image_path, class_name):
     bboxes = []
     for detection in detections:
         for obj in detection:
+           for obj in detection:
             scores = obj[5:]
+            if not scores:
+                continue
             class_id = np.argmax(scores)
             confidence = scores[class_id]
             if confidence > 0.3:  # Prueba con un umbral más bajo (por ejemplo, 0.3)
@@ -74,4 +77,30 @@ def save_cropped_images(image_path, bboxes):
             print(f"Advertencia: Cuadro delimitador inválido para la detección {i+1} en la imagen '{image_path}'")
         else:
             cropped_img_path = os.path.join(cropped_images_folder, f"{os.path.splitext(os.path.basename(image_path))[0]}_{i}.jpg")
-           
+            cv2.imwrite(cropped_img_path, cropped_img)
+
+
+# Recorrer todas las imágenes en cada carpeta de especies
+for class_name in os.listdir(dataset_folder):
+    class_folder = os.path.join(dataset_folder, class_name)
+    class_destination_folder = os.path.join(destination_folder, class_name)
+
+    os.makedirs(class_destination_folder, exist_ok=True)
+
+    for image_name in os.listdir(class_folder):
+        image_path = os.path.join(class_folder, image_name)
+        class_labels, bboxes = detect_objects(image_path, class_name)
+
+        # Obtener las dimensiones de la imagen
+        image = cv2.imread(image_path)
+        height, width = image.shape[:2]
+
+        # Guardar imágenes en la carpeta "images"
+        destination_image_path = os.path.join(images_folder, image_name)
+        shutil.copy(image_path, destination_image_path)
+
+        # Guardar etiquetas y cuadros delimitadores en formato YOLO
+        # Aquí debes implementar la función save_labels_and_bboxes si es necesaria
+
+        # Guardar imágenes recortadas en la carpeta "cropped_images"
+        save_cropped_images(image_path, bboxes)
